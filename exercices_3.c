f20cf8f3badfb3b8c843b2e061a2c5b490384540
Josua Gonzalez <jdpepi@gmail.com>
2016-01-22 16:56:07 +0100

    Update exercices_3.c

--- exercices_3.c
+++ exercices_3.c
@@ -1,6 +1,6 @@
-/* Exercices feuille 3 - de 1 à 6 */
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 int tabLen = 8;
 int tab[8] = {12, 15, 13, 10, 8, 9, 13, 14};
@@ -87,7 +87,6 @@ void f3e4_searchReplace() {
     dispTab(tab, tabLen);
 }
 
-
 // swap
 void f3e5_swap() {
     int input1, input2, buffer;
@@ -126,8 +125,227 @@ void f3e6_sort() {
     f3e2_reverseTab();
 }
 
+/* PART 2 */
 
-int main() {
+int f3e7_isPalindrome(char string[], int strLen) {
+    int i;
+    char stringRev[strLen];
+
+    for (i = 0; i < strLen - 1; i++) {
+        stringRev[i] = string[strLen - i - 2];
+    }
+    stringRev[strLen - 1] = '\0';
+
+    return (strcmp(string, stringRev) == 0) ? 1 : 0;
+}
+
+int f3e8_strLen(char string[]) {
+    int i = 0;
+    for (; i < INT_MAX; i++) {
+        if (string[i] == '\0') {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+void f3e8() {
+    char str[100];
+    printf("Input string: ");
+    gets(str);
+    printf("Length: %d", f3e8_strLen(str));
+}
+
+void f3e9_strConcat(char *output, char str1[], char str2[]) {
+    int i, offset;
+    for (i = 0; i < f3e8_strLen(str1); i++) {
+        output[i] = str1[i];
+    }
+    offset = i;
+    for (i = 0; i < f3e8_strLen(str2); i++) {
+        output[i + offset] = str2[i];
+    }
+}
+
+void f3e9() {
+    char str1[100], str2[100], output[200];
+    gets(str1);
+    gets(str2);
+    f3e9_strConcat(output, str1, str2);
+    printf("%s", output);
+}
+
+int f3e10_strEquals(char str1[], char str2[]) {
+    int len1 = f3e8_strLen(str1);
+    int len2 = f3e8_strLen(str2);
+
+    if (len1 != len2) {
+        return 0;
+    }
+
+    int i;
+    for (i = 0; i < len1; i++) {
+        if (str1[i] != str2[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void f3e10() {
+    char str1[100], str2[100];
+    printf("1: ");
+    scanf("%s", str1);
+    printf("2: ");
+    scanf("%s", str2);
+    printf("%d", f3e10_strEquals(str1, str2));
+}
+
+int f3e10_strEqualsLim(char str1[], char str2[], int limit) {
+    int len1 = f3e8_strLen(str1);
+    int len2 = f3e8_strLen(str2);
+
+    if (limit > len1 && limit > len2 && len1 != len2) {
+        return 0;
+    }
+
+    if (limit > len1) {
+        limit = len1;
+    }
+
+    int i;
+    for (i = 0; i < limit; i++) {
+        if (str1[i] != str2[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void f3e11() {
+    char str1[100], str2[100];
+    int input;
+
+    printf("str1: ");
+    scanf("%s", str1);
+    printf("str2: ");
+    scanf("%s", str2);
+    printf("int: ");
+    scanf("%d", &input);
+
+    printf("%d", f3e10_strEqualsLim(str1, str2, input));
+}
+
+int f3e12_strStr(char needle[], char haystack[]) {
+    int lenN = f3e8_strLen(needle);
+    int lenH = f3e8_strLen(haystack);
+    int i, matchCount = 0;
+
+    for (i = 0; i < lenH; i++) {
+        if (lenH < lenN + i) {
+            return -1;
+        }
+
+        if (haystack[i] == needle[matchCount]) {
+            matchCount++;
+        } else {
+            matchCount = 0;
+        }
+
+        if (matchCount == lenN) {
+            return i - matchCount + 1;
+        }
+    }
+
+    return -1;
+}
+
+int f3e12_strFind(char needle[], char haystack[]) {
+    return f3e12_strStr(needle, haystack) >= 0 ? 1 : 0;
+}
+
+void f3e12() {
+    char str1[100], str2[100];
+
+    printf("str1: ");
+    scanf("%s", str1);
+    printf("str2: ");
+    scanf("%s", str2);
+
+
+    printf("%d", f3e12_strFind(str2, str1));
+}
+
+void f3e13_strOffset(char *string, char offset) {
+    int i;
+    for (i = 0; i < f3e8_strLen(string); i++) {
+        string[i] = string[i] + offset;
+    }
+}
+
+void f3e13() {
+    char str[100];
+
+    printf("input: ");
+    scanf("%s", str);
+
+    f3e13_strOffset(str, -32);
+    printf("%s", str);
+}
+
+void f3e14() {
+    char str[100];
+
+    printf("input: ");
+    scanf("%s", str);
+
+    f3e13_strOffset(str, 32);
+    printf("%s", str);
+}
+
+void f3e15() {
+    char str[100];
+
+    printf("input: ");
+    scanf("%s", str);
+
+    char *lim = strstr(str, "er");
+    if (lim != NULL) {
+        strcpy(lim, "\0");
+
+        printf("je %se\n", str);
+        printf("tu %ses\n", str);
+        printf("il %se\n", str);
+        printf("nous %sons\n", str);
+        printf("vous %sez\n", str);
+        printf("ils %sent\n", str);
+    }
+
+    printf("%s", str);
+}
+
+void f3e16() {
+    char str1[100], str2[100];
+
+    printf("str1: ");
+    scanf("%s", str1);
+    printf("str2: ");
+    scanf("%s", str2);
+
+    int mid1 = f3e8_strLen(str1) / 2;
+    int mid2 = f3e8_strLen(str2) / 2;
+
+    strcpy(&str1[mid1], "\0");
+
+    char *str3 = strcat(str1, &str2[mid2]);
+
+    printf("%s", str3);
+}
+
+void part1() {
     printf("Original tab: ");
     dispTab(tab, tabLen);
 
@@ -137,6 +355,21 @@ int main() {
 //    f3e4_searchReplace();
 //    f3e5_swap();
 //    f3e6_sort();
+}
+
+int main() {
+//    part1();
+
+//    printf("%d", f3e7_isPalindrome("laval", 6));
+//    f3e8();
+//    f3e9();
+//    f3e10();
+//    f3e11();
+//    f3e12();
+//    f3e13();
+//    f3e14();
+//    f3e15();
+//    f3e16();
 
     return 0;
 }
